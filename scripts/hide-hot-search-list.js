11af08720b917ae002a5cb98923e16f74f8a658a
wanglq <535346881@qq.com>
2024-03-24 17:03:58 +0800

    fix: hide hot search list does not work in homePage and search bar height error when history record is empty

--- scripts/hide-hot-search-list.js
+++ scripts/hide-hot-search-list.js
@@ -3,8 +3,18 @@
  */
 chrome.storage.sync.get(["biliplus-enable", "hide-hot-search-list"], (storage) => {
   if (storage["biliplus-enable"] && storage["hide-hot-search-list"]) {
-    const navSearch = document.querySelector('#biliMainHeader')
+    const header = document.querySelector(location.href.includes('bilibili.com/video') ? '#biliMainHeader' : ".bili-header");
+    header.addEventListener('focusin', () => {
+      const history = document.querySelector('.search-panel .history')
+      const searchPanel = document.querySelector('.search-panel')
+      const navSearchform = document.querySelector('#nav-searchform')
+      if (!history) {
+        searchPanel.style.display = 'none'
+        navSearchform.style.borderRadius = '8px'
+
+      }
+    })
+    const navSearch = document.querySelector('body')
     navSearch.classList.add('biliplus-hide-hot-search-list')
-    console.log('hide-hot-search-list enabled')
   }
 });
