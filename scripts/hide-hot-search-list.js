94b4d5efbb93fbb85b447d1070b090f37d0aea23
wanglq <535346881@qq.com>
2024-03-24 16:42:33 +0800

    fix: hide hot search list does not work in play video page

--- scripts/hide-hot-search-list.js
+++ scripts/hide-hot-search-list.js
@@ -3,7 +3,8 @@
  */
 chrome.storage.sync.get(["biliplus-enable", "hide-hot-search-list"], (storage) => {
   if (storage["biliplus-enable"] && storage["hide-hot-search-list"]) {
-    const navSearch = document.querySelector('.search-panel')
+    const navSearch = document.querySelector('#biliMainHeader')
     navSearch.classList.add('biliplus-hide-hot-search-list')
+    console.log('hide-hot-search-list enabled')
   }
 });
