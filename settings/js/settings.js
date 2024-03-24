a68f4f6ee155eedd695add4614d7a8f00e2c76f8
Timothy Lau <timothy-lau@outlook.com>
2024-03-24 14:58:20 +0800

    修改 AI 总结触发逻辑、格式化代码

--- settings/js/settings.js
+++ settings/js/settings.js
@@ -1,29 +1,26 @@
 $(function () {
-
-    $("input[type=checkbox]").each(function(){
-
-        let key = $(this).data("key");
-        chrome.storage.sync.get(key, (storage) => {
-            $(this).attr("checked", storage[key]);
-        })
-
-        $(this).change(() => {
-            const key = $(this).data("key");
-            let checked = $(this).is(":checked");
-            const data = {};
-            data[key] = checked;
-            chrome.storage.sync.set(data, () => {
-                console.log("配置保存成功！");
-            })
-        });
+  $('input[type=checkbox]').each(function () {
+    let key = $(this).data('key');
+    chrome.storage.sync.get(key, storage => {
+      $(this).attr('checked', storage[key]);
     });
 
-    chrome.storage.sync.get("hide-user-comment", (storage) => {
+    $(this).change(() => {
+      const key = $(this).data('key');
+      let checked = $(this).is(':checked');
+      const data = {};
+      data[key] = checked;
+      chrome.storage.sync.set(data, () => {
+        console.log('配置保存成功！');
+      });
+    });
+  });
 
-        const hideUsers = storage["hide-user-comment"];
-        if (hideUsers != null && hideUsers.length > 0){
-            $("#hide-user-comment-count").html(hideUsers.length);
-            $("#hide-user-comment-count").removeClass("hidden");
-        }
-    })
+  chrome.storage.sync.get('hide-user-comment', storage => {
+    const hideUsers = storage['hide-user-comment'];
+    if (hideUsers != null && hideUsers.length > 0) {
+      $('#hide-user-comment-count').html(hideUsers.length);
+      $('#hide-user-comment-count').removeClass('hidden');
+    }
+  });
 });
